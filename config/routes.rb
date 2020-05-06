1cec24e9640c62463c872841f252781b00c2f353
vagrant <vagrant@localhost.localdomain>
2020-05-06 13:32:40 +0000

    about_error

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
+  root 'homes#top'
   devise_for :users
   resources :users, only:[:index, :show, :edit, :update, :index]
   resources :books
+
+  get "about" => "homes#about"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
